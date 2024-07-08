4bb26ac1995dcf1baec3f0155783457220597592
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2024-07-08 18:54:50 +0100

    Images

--- products/apps.py
+++ products/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class ProductsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    # default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
-    label = 'unique_products' # Make sure this label is unique if there are multiple product-related apps
+
